cf76c62409ea4a88b8b5945252c743f591457a06
Diego Magalhaes <brandaodiegof@gmail.com>
2021-06-22 12:57:20 -0300

    Segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,13 @@
+import "reflect-metadata";
 import express, { response } from "express";
+import { router } from "./routers";
 
+import "./database";
 //@types/express -D
 const app = express();
-/**
- * GET    => Buscar uma informação
- * POST   => inserir(criar) uma informação
- * PUT    => Alterar informação
- * DELETE => Deletar um dado
- * PATCH  => Alterar uma informação especifica
- */
 
-app.get("/teste", (request, response) => {
-    // Request => tudo que está entrando
-    // Response => tudo que está saindo
-    return response.send("Olá NLW")
-})
+app.use(express.json());
 
-app.post("/teste-post", (request, response) => {
-    return response.send("OLÁ NLW método POST!")
-})
+app.use(router);
 
-// http://localhost:3000
-app.listen(3000, () => console.log("Server is Running NLW"))
+app.listen(3000, () => console.log("Server is Running"))
